162a684c7b30b13cb423c4136b97da47128398ff
CHV - Soluções em TI. <chvsolucoes@gmail.com>
2026-09-04 08:46:59 -0300

    Publish CHV Remote PWA on gh-pages

--- src/compat/linux/mmc/host.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
-#ifndef _RTW88_COMPAT_MMC_HOST_H
-#define _RTW88_COMPAT_MMC_HOST_H
-
-#include "../sdio.h"
-
-#endif /* _RTW88_COMPAT_MMC_HOST_H */
